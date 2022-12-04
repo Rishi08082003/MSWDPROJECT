dd5441f060c280a7a069c8749ccc4157619c6df3
Rishi08082003 <110282035+Rishi08082003@users.noreply.github.com>
2022-12-04 09:13:09 +0530

    Add files via upload

---
+++ abouts.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import video1 from "./images/vi1.mp4";
+const abouts = () => {
+return (
+	<div>
+
+		<html>
+<head>
+<title>STOCK MANAGEMENT SYSTEM</title>
+</head>
+
+</html>
+<video src={video1} width="600" height="300"  controls="controls" autoplay="true" />
+	</div>
+);
+};
+
+export default abouts;
