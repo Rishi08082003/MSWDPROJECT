da6de3f07592f6f96a372b7ff3a785d02c2373d3
Rishi08082003 <110282035+Rishi08082003@users.noreply.github.com>
2022-12-04 09:14:50 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+const hStyle2 = { color: 'red' };
+const hStyle1 = { color: 'blue' };
+
+const hStyle4 = { color: 'black' };
+const Contact = () => {
+return (
+	<div>
+	<h1 style={ hStyle4 }><b>OFFICIAL CONTACT INFORMATION:-</b></h1>
+	             <h1 style={hStyle2}>Mail US: <h style={hStyle1}>vnvrkreddy@gmail.com</h></h1>
+	<h1 style={hStyle2}>FOR CONTACT AND TEXT:<h style={hStyle1}>6281433341</h></h1>
+	              <h1 style={hStyle2}>Adress:<h style={hStyle1}>vijayawada</h></h1>
+	    <h1 style={hStyle2}>FOR MORE QUERIES:<h style={hStyle1}>MEET OUR TEAM MARKETING MANAGER VAMSI</h></h1>
+
+	</div>
+);
+};
+
+export default Contact;
